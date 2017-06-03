3704a0909e6768601aaba8e0b2311f6e0b3be41b
Vikram Tiwari <vikramtheone1@gmail.com>
2017-06-02 23:25:02 -0700

    use standard formatter

--- test/specs/menu.spec.js
+++ test/specs/menu.spec.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 
 var chai = require('chai'),
-  expect = chai.expect;
+  expect = chai.expect
 
-var Menus = require('../../lib/core_modules/menu');
+var Menus = require('../../lib/core_modules/menu')
 
 describe('Menus', function () {
-  describe('Functions', function() {
-    it('should have expected functions', function() {
-      expect(Menus.prototype.add).to.exist;
-      expect(Menus.prototype.get).to.exist;
-    });
-  });
-});
+  describe('Functions', function () {
+    it('should have expected functions', function () {
+      expect(Menus.prototype.add).to.exist
+      expect(Menus.prototype.get).to.exist
+    })
+  })
+})
--- test/specs/menuItem.spec.js
+++ test/specs/menuItem.spec.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 
 var chai = require('chai'),
-  expect = chai.expect;
+  expect = chai.expect
 
-var MenuItem = require('../../lib/core_modules/menu/MenuItem.js');
+var MenuItem = require('../../lib/core_modules/menu/MenuItem.js')
 
 describe('MenuItem', function () {
   it('should have functions', function () {
-    expect(MenuItem.hasRole).to.exist;
+    expect(MenuItem.hasRole).to.exist
 
-    expect(MenuItem.prototype.strip).to.exist;
-    expect(MenuItem.prototype.props).to.exist;
-    expect(MenuItem.prototype.findOrCreate).to.exist;
-    expect(MenuItem.prototype.list).to.exist;
-    expect(MenuItem.prototype.get).to.exist;
-    expect(MenuItem.prototype.add).to.exist;
-  });
-});
+    expect(MenuItem.prototype.strip).to.exist
+    expect(MenuItem.prototype.props).to.exist
+    expect(MenuItem.prototype.findOrCreate).to.exist
+    expect(MenuItem.prototype.list).to.exist
+    expect(MenuItem.prototype.get).to.exist
+    expect(MenuItem.prototype.add).to.exist
+  })
+})
